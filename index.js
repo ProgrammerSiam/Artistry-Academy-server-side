5211a23796ee39d8169796ff7fb8341de84e039e
Siam10101 <mehedihasansiam030@gmail.com>
2023-06-17 20:09:54 +0600

    some customize

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const verifyJWT = (req, res, next) => {
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.tlwiikw.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
